a355a0f8905426aaae5d81f3e32efc8a26917ec6
Michał Czyż <m.czyz@selleo.com>
2016-10-22 08:30:59 +0200

    ember g model Game lastMove field_1 field_2 field_3 field_4 field_5 field_6 field_7 field_8 field_9

---
+++ app/models/game.js
@@ -0,0 +1,14 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  lastMove: DS.attr(),
+  field1: DS.attr(),
+  field2: DS.attr(),
+  field3: DS.attr(),
+  field4: DS.attr(),
+  field5: DS.attr(),
+  field6: DS.attr(),
+  field7: DS.attr(),
+  field8: DS.attr(),
+  field9: DS.attr()
+});
